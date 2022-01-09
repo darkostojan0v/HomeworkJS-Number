e9af6b6b94b31935f54f1e682f1ba02591124ddb
darkostojan0v <95142705+darkostojan0v@users.noreply.github.com>
2022-01-09 14:58:35 +0100

    Add files via upload

---
+++ Number in Array/script.js
@@ -0,0 +1,7 @@
+console.log('Number in Array');
+
+function numberInArray() {
+    let container = ['6', '17', '23', '67', '89'];
+    return container.includes('23');
+}
+console.log(numberInArray());
